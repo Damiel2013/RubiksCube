8d04fb0f21fc08d43944da735cff6086e0809d63
Fesqvw <Fesqvw@192.168.1.41>
2019-03-18 21:05:00 +0100

    pipo

---
+++ Rubik's cube/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+	
+	public static int[][] rubik = new int[6][9];
+	
+	
+//executable----------------------------------------------------------------------------------------------------------------------------------------
+	public static void main(String[] args) {
+		
+		CubeRubik.remplissage(rubik);
+		CubeRubik.show(rubik);
+		Moves.scrumble(rubik);
+		CubeRubik.show(rubik);
+		
+	}
+
+}
+
+
+
+		//blue:			0
+		//orange:		1
+		//white:		2
+		//green:		3
+		//red:			4
+		//yellow:		5
+	
